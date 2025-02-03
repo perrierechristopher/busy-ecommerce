b75382af63b2d73323a02913b81da3fe8877ff4d
Dany Christopher PERRIERE <perrierechristopher@gmail.com>
2025-02-03 15:00:28 +0000

    First commit

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,5 +2,6 @@ import { revalidate } from 'lib/shopify';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-  return revalidate(req);
+    // return revalidate(req);
+    return Promise.resolve(NextResponse.json({"message": "ok"}))
 }
