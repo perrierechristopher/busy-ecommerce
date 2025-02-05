db6a566fff53b526290a3f273c7add486280067c
Dany Christopher PERRIERE <perrierechristopher@gmail.com>
2025-02-05 14:39:03 +0000

    Component creatd

--- components/layout/breadcrumb.tsx
+++ components/layout/breadcrumb.tsx
@@ -1,11 +1,30 @@
+'use client'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
-type Props = {}
+const BreadCrumbs = () => {
+    const pathname = usePathname()
+    const pathSegments = pathname.split("/").filter(Boolean);
 
-const breadcrumb = (props: Props) => {
-  return (
-    <div>breadcrumb</div>
-  )
+    return (
+        <nav className="text-sm px-4 pb-4">
+          <ol className="flex space-x-2">
+            <li>
+              <Link href="/" className="text-white text-lg">Home</Link>
+            </li>
+            {pathSegments.map((segment, index) => {
+              const href = "/" + pathSegments.slice(0, index + 1).join("/");
+              return (
+                <li key={href} className="flex items-center space-x-2">
+                  <span className='text-lg'>{">"}</span>
+                  <Link href={href} className="text-white text-lg capitalize">{segment}</Link>
+                </li>
+              );
+            })}
+          </ol>
+        </nav>
+      );
 }
 
-export default breadcrumb
+export default BreadCrumbs
