4aadc9d95d2543e742df8073b77551aa214cac71
Dany Christopher PERRIERE <perrierechristopher@gmail.com>
2025-02-08 22:41:42 +0000

    Fixed opengraph-image file

--- components/opengraph-image.tsx
+++ components/opengraph-image.tsx
@@ -1,17 +1,20 @@
 import { ImageResponse } from 'next/og';
 import LogoIcon from './icons/logo';
+import localFont from 'next/font/local';
+
+const inter = localFont({
+  src: '../fonts/Inter-Bold.ttf', 
+  display: 'swap',
+  weight: '700',
+});
+
 
 export type Props = {
   title?: string;
 };
 
 export default async function OpengraphImage(props?: Props): Promise<ImageResponse> {
-  const { title } = {
-    ...{
-      title: process.env.SITE_NAME
-    },
-    ...props
-  };
+  const { title } = { ...{ title: process.env.SITE_NAME }, ...props };
 
   return new ImageResponse(
     (
@@ -19,22 +22,15 @@ export default async function OpengraphImage(props?: Props): Promise<ImageRespon
         <div tw="flex flex-none items-center justify-center border border-neutral-700 h-[160px] w-[160px] rounded-3xl">
           <LogoIcon width="64" height="58" fill="white" />
         </div>
-        <p tw="mt-12 text-6xl font-bold text-white">{title}</p>
+        <p tw="mt-12 text-6xl font-bold text-white" style={{ fontFamily: inter.style.fontFamily }}>
+          {title}
+        </p>
       </div>
     ),
     {
       width: 1200,
       height: 630,
-      fonts: [
-        {
-          name: 'Inter',
-          data: await fetch(new URL('../fonts/Inter-Bold.ttf', import.meta.url)).then((res) =>
-            res.arrayBuffer()
-          ),
-          style: 'normal',
-          weight: 700
-        }
-      ]
     }
   );
 }
+
